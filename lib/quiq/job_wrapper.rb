16967cf01c11ee62ff42b9a324e91ae158939009
Salim Semaoune <salim.semaoune@qonto.eu>
2020-01-27 22:19:10 +0100

    rename @item into @job

--- lib/quiq/job_wrapper.rb
+++ lib/quiq/job_wrapper.rb
@@ -5,17 +5,17 @@ require_relative 'test_job' # TODO: remove this dependency
 
 module Quiq
   class JobWrapper
-    def initialize(item)
+    def initialize(job)
       # TODO: handle deserialization errors
-      @item = JSON.parse(item) rescue nil
+      @job = JSON.parse(job) rescue nil
     end
 
     def run
-      return if @item.nil?
+      return if @job.nil?
 
       Async do
-        klass = Object.const_get(@item['wrapped'])
-        args = @item['args'].first['arguments']
+        klass = Object.const_get(@job['wrapped'])
+        args = @job['args'].first['arguments']
         klass.new.perform(*args)
       end
     end
