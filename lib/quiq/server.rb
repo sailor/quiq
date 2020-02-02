f05d5e289f77789f63b878df6011c33c1d405959
Dmitry <dmitry.chopey@gmail.com>
2020-02-02 20:24:52 +0100

    Add signal handling (draft)

--- lib/quiq/server.rb
+++ lib/quiq/server.rb
@@ -11,14 +11,48 @@ module Quiq
     def run
       @queues = Quiq.queues.map { |q| "queue:#{q}" }
 
-      Async do
-        loop do
-          job = fetch_one
-          Worker.new(job).run
+      self_read, self_write = IO.pipe
+      sigs = %w[INT TERM TTIN TSTP]
+      sigs.each do |sig|
+        trap sig do
+          self_write.puts(sig)
         end
-      ensure
-        Quiq.redis.close
+      rescue ArgumentError
+        puts "Signal #{sig} not supported"
       end
+
+      Async do 
+        polling_task = Async do
+          loop do
+            job = fetch_one
+            Worker.new(job).run
+          end
+        ensure
+          Quiq.redis.close
+        end
+  
+        begin
+          while (readable_io = IO.select([self_read]))
+            signal = readable_io.first[0].gets.strip
+
+            case signal
+            # INT - Ctrl-C in terminal
+            # TERM - Termination of Quiq process
+            when 'INT', 'TERM'
+              raise Interrupt 
+            # TSTP - Is used to stop polling new jobs. 
+            # Call TSTP and then TERM in order to gracefully shutdown Quiq
+            when 'TSTP'
+              puts "Stop polling new jobs!"
+              polling_task.stop
+            end
+          end
+        rescue Interrupt
+          puts "\n Shutting down"
+          exit(0)
+        end
+      end
+
     end
 
     def fetch_one
