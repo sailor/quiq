205c8d1cb571747fc9766f932cf277dcdb058820
Salim Semaoune <salim.semaoune@qonto.eu>
2020-02-06 23:31:53 +0100

    Add zombie jobs recovery

--- lib/quiq/worker.rb
+++ lib/quiq/worker.rb
@@ -6,9 +6,16 @@ module Quiq
   class Worker
     def initialize(queue)
       @queue = queue
+      @processing_queue = "#{@queue}:processing"
     end
 
     def start
+      # Purge the processing queue by re-enqueing
+      # messages that weren't fully processed
+      # beware that your jobs must be idempotent!
+      purge_processing_queue!
+
+      # Then start processing enqueued jobs
       Async do
         loop do
           job = fetch_one
@@ -20,11 +27,31 @@ module Quiq
       end
     end
 
+    private
+
     def fetch_one
       # BRPOPLPUSH pops a job from the working queue
       # then put it in a processing queue to ensure
       # an "at least once" behaviour
-      Quiq.redis.brpoplpush(@queue, "#{@queue}:processing", 0).last
+      Quiq.redis.brpoplpush(@queue, @processing_queue, 0).last
+    end
+
+    # Insert elements that weren't fully processed
+    # at the tail of the queue to avoid loss
+    # Note that they should be enqueued at the head
+    # of the queue, but Redis lacks a LPOPRPUSH command
+    def purge_processing_queue!
+      task = Async do
+        Quiq.redis.pipeline do |pipe|
+          loop do
+            job = pipe.sync.call('RPOPLPUSH', @processing_queue, @queue)
+            break if job.nil?
+          end
+          pipe.close
+        end
+      end
+
+      task.wait
     end
   end
 end
