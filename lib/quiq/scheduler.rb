200a5022c9fd01f3858f2c9917750e476a5b57bc
Salim Semaoune <salim.semaoune@qonto.eu>
2020-02-12 22:59:02 +0100

    Put the throttling at the beginning of the loop as it seems to be way more efficient

--- lib/quiq/scheduler.rb
+++ lib/quiq/scheduler.rb
@@ -13,11 +13,12 @@ module Quiq
 
       Async do
         loop do
+          sleep 0.2
+
           serialized_job, scheduled_at = SchedulerQueue.pull
           next unless serialized_job && time_to_process?(scheduled_at)
 
           SchedulerQueue.move_to_original_queue(serialized_job)
-          sleep 0.2
         end
       ensure
         Quiq.redis.close
