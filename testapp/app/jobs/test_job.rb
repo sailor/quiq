a4ed318842029bbd6784e58989d7dbc71359aadd
Baicheng Yu <baicheng.yu@qonto.eu>
2020-02-16 17:38:16 +0100

    Add retry example to testapp

--- testapp/app/jobs/test_job.rb
+++ testapp/app/jobs/test_job.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 class TestJob < ApplicationJob
+  class CustomError < StandardError; end
+
+  retry_on(CustomError, wait: 5, attempts: 3, queue: :retry) do
+    raise
+  end
+
   def perform(data, wait)
     puts "[Worker ##{$$}] Receiving new job: #{data}"
     Quiq.current_task.sleep wait
     puts "[Worker ##{$$}] Time to wake up after #{wait} seconds"
+    raise CustomError, 'Caught CustomError'
   end
 end
